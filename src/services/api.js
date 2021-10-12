f40517542ad2d32fb77ecbcc55f3069e799af36f
YanMaciel <araujo.yan@hotmail.com>
2021-10-12 15:55:12 -0300

    configs DNS route

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://notesapp-api-app.herokuapp.com/',
+  baseURL: 'http://notesapi.ecdb.com.br/',
 });
 
 export default api;
